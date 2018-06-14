d5759cb770b7d7bceaf19933f4e0c3e3e9756810
Bo Chen <bochen2014@yahoo.com>
2018-06-14 21:28:20 +1000

    update

--- client/src/components/record.js
+++ client/src/components/record.js
@@ -1,11 +1,15 @@
 import React from "react";
+import classNames from "classnames/bind";
+import styles from "./record.less";
 
+const cx = classNames.bind(styles);
 const Record = props => {
   const { index, word, pronouncation, explaination } = props;
+  const pronc=pronouncation.slice(1,-1);
   return (
-    <div>
-      <div>{word}</div>
-      <div>{pronouncation}</div>
+    <div id={`_id-${index}`} className={cx("wrapper")}>
+      <h2>{word}</h2>
+      {pronc && <div>/{pronc}/</div>}
       <div>
         <pre>{explaination}</pre>
       </div>
