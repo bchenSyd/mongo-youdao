c681a6d85df7e0cb0e84650fa5c009caf36e13c7
Bo Chen <bo.chen@westpac.com.au>
2018-06-14 14:14:03 +1000

    ssh tunnelling

--- server/db/index.js
+++ server/db/index.js
@@ -1,28 +1,21 @@
-import { MongoClient, ObjectID } from 'mongodb';
-import { connection_str } from './config';
-
+const { MongoClient, ObjectID } = require("mongodb");
+const { connection_str } = require("./config");
 
 let db;
 const connect = async () => {
-    if (!db) {
-        db = await MongoClient.connect(connection_str);
-        console.log(`mongodb connected to ${connection_str}`);
-    }
+  if (!db) {
+    db = await MongoClient.connect(connection_str);
+    console.log(`mongodb connected to ${connection_str}`);
+  }
 };
 
 const get = () => db;
 
 const close = async () => {
-    if (db) {
-        await db.close();
-        console.log(`connection to mongodb ${connection_str} closed successfully`);
-    }
+  if (db) {
+    await db.close();
+    console.log(`connection to mongodb ${connection_str} closed successfully`);
+  }
 };
 
-
-export {
-    get,
-    connect,
-    close,
-    ObjectID,
-};
+module.exports = { get, connect, close, ObjectID };
