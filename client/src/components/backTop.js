3d717e99d674d79249f1abec4a3956ef5dbcc0f7
Bo Chen <bo.chen@westpac.com.au>
2018-06-19 14:56:06 +1000

    update

--- client/src/components/backTop.js
+++ client/src/components/backTop.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { createRef, Component } from "react";
 import classNames from "classnames/bind";
 import raf from "raf";
 import styles from "./backTop.less";
@@ -15,10 +15,10 @@ const easeInOutCubic = (t, b, c, d) => {
 
 const cx = classNames.bind(styles);
 class BackTop extends Component {
-  state = {
-    visible: false
-  };
+  ref = createRef();
+
   componentDidMount() {
+    this.footerNode = document.querySelector("footer");
     window.addEventListener("scroll", this.handleScroll);
     this.handleScroll();
   }
@@ -27,11 +27,11 @@ class BackTop extends Component {
     window.removeEventListener("scroll", this.handleScroll);
   }
 
-  getCurrentScrollTop = () => {
+  getScrollOffset = () => {
     return (
-      window.pageYOffset ||
-      document.body.scrollTop ||
-      document.documentElement.scrollTop
+      window.pageYOffset || //most browsers except IE before #9
+      document.body.scrollTop || // IE 'quirks'
+      document.documentElement.scrollTop // IE with 'doctype'
     );
   };
 
@@ -41,13 +41,26 @@ class BackTop extends Component {
   }
 
   handleScroll = () => {
-    this.setState({
-      visible: this.getCurrentScrollTop() > 400
-    });
+    const footerClientRect = this.footerNode.getBoundingClientRect();
+    const windowClientHeight = document.body.clientHeight;
+
+    const style = this.ref.current.style;
+    style.display = this.getScrollOffset() > 400 ? "block" : "none";
+    if (footerClientRect.top < windowClientHeight) {
+      // footer is visible
+      style.position = "absolute";
+      style.bottom = "";
+      style.top = this.getScrollOffset() + footerClientRect.top - 90 + "px";
+    } else {
+      // footer is invisible; backTop should fix to viewport bottom
+      style.position = "fixed";
+      style.bottom = "50px";
+      style.top = "";
+    }
   };
 
   scrollToTop = () => {
-    const scrollTop = this.getCurrentScrollTop();
+    const scrollTop = this.getScrollOffset();
     const startTime = Date.now();
     const frameFunc = () => {
       const timestamp = Date.now();
@@ -61,14 +74,11 @@ class BackTop extends Component {
   };
 
   render() {
-    const { visible } = this.state;
     return (
-      <div className={cx("backtop")}>
-        {visible && (
-          <div className={cx("backtop-content")} onClick={this.scrollToTop}>
-            <div className={cx("backtop-icon")} />
-          </div>
-        )}
+      <div ref={this.ref} className={cx("backtop")}>
+        <div className={cx("backtop-content")} onClick={this.scrollToTop}>
+          <div className={cx("backtop-icon")} />
+        </div>
       </div>
     );
   }
